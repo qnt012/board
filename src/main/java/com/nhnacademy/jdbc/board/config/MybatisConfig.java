f316a871284731d410ca8d318c820a623d7e7457
김보민 <qnt012@naver.com>
2022-05-18 15:21:28 +0900

    setting

--- src/main/java/com/nhnacademy/jdbc/board/config/MybatisConfig.java
+++ src/main/java/com/nhnacademy/jdbc/board/config/MybatisConfig.java
@@ -19,9 +19,9 @@ public class MybatisConfig {
     public DataSource dataSource(){
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setDriverClassName(com.mysql.cj.jdbc.Driver.class.getName());
-        basicDataSource.setUrl("jdbc:mysql://133.186.211.156:3306/nhn_academy_32");
-        basicDataSource.setUsername("nhn_academy_32");
-        basicDataSource.setPassword("EJdrW!(bf]HjT9a5");
+        basicDataSource.setUrl("jdbc:mysql://133.186.211.156:3306/nhn_academy_5");
+        basicDataSource.setUsername("nhn_academy_5");
+        basicDataSource.setPassword("1Nv@Fbn6IU94!8EJ");
         basicDataSource.setInitialSize(2);
         basicDataSource.setMaxTotal(10);
         return basicDataSource;
