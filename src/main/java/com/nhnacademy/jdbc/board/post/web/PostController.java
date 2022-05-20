320dc1f4cbcd3e33d5da7f3053457b0a484ccf27
김보민 <qnt012@naver.com>
2022-05-20 23:16:08 +0900

    Controller 분리

--- src/main/java/com/nhnacademy/jdbc/board/post/web/PostController.java
+++ src/main/java/com/nhnacademy/jdbc/board/post/web/PostController.java
@@ -87,15 +87,6 @@ public class PostController {
         return "redirect:/postView/0";
     }
 
-    @PostMapping("/commentInsert/{postNum}")
-    public String postPostModify(@PathVariable long postNum,
-                                 @RequestParam String commentContent,
-                                 HttpSession session) {
-        User user = (User) session.getAttribute("login");
-        commentService.createComment(postNum, user.getUserNum(), commentContent);
-        return "redirect:/postDetail/{postNum}";
-    }
-
     @GetMapping("/postDeleteList/{page}")
     public String getPostDeleteList(@PathVariable int page, ModelMap modelMap){
         modelMap.put("postViews", postService.viewPosts(page));
@@ -109,24 +100,5 @@ public class PostController {
     public String getPostRestore(@PathVariable long postNum) {
         postService.restorePost(postNum);
         return "redirect:/postDeleteList/0";
-    }
-
-    @GetMapping("/commentModify/{commentNum}")
-    public String getCommentModify(@PathVariable long commentNum, ModelMap modelMap) {
-        modelMap.put("comment", commentService.getComment(commentNum));
-        return "commentModifyForm";
-    }
-
-    @PostMapping("/commentModify/{commentNum}")
-    public String postCommentModify(@PathVariable long commentNum,
-                                   @RequestParam String commentContent) {
-        long postNum = commentService.modifyComment(commentNum, commentContent);
-        return "redirect:/postDetail/"+postNum;
-    }
-
-    @GetMapping("/commentDelete/{commentNum}")
-    public String getCommentDelete(@PathVariable long commentNum) {
-        long postNum = commentService.removeComment(commentNum);
-        return "redirect:/postDetail/"+postNum;
     }
 }
